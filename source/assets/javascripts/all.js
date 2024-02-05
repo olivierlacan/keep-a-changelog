831becb73e33303f6d54d23a68c2504d58a30da9
Olivier Lacan <hi@olivierlacan.com>
2024-02-04 17:08:12 -0800

    Always show FAQ answers

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -5,30 +5,4 @@ document.addEventListener("DOMContentLoaded", function(){
     languageCode = event.currentTarget.selectedOptions[0].value
     window.location.replace(origin + "/" + languageCode)
   });
-
-  var faqHeaders = document.querySelectorAll('.frequently-asked-questions h4');
-
-  for (var i = 0; i < faqHeaders.length; ++i) {
-    header = faqHeaders[i]
-    header.addEventListener('click', toggleVisibility);
-  }
-
-  function toggleVisibility(event) {
-    element = event.target;
-    element.classList.toggle('is-visible')
-
-    lastParagraph = element.nextElementSibling;
-    paragraphs = [];
-    paragraphs.push(lastParagraph);
-
-    while (lastParagraph.nextElementSibling && lastParagraph.nextElementSibling.tagName == "P") {
-      lastParagraph = lastParagraph.nextElementSibling;
-      paragraphs.push(lastParagraph)
-    }
-
-    for (var i = 0; i < paragraphs.length; ++i) {
-      paragraph = paragraphs[i]
-      paragraph.classList.toggle('is-visible');
-    }
-  }
 });
