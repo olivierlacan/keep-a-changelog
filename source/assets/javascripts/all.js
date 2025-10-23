fe852a1cf217ab0de0ae9f3babf384ee3efa3a58
hdydgsgg58-svg <hdydgsgg58@gmail.com>
2025-10-23 19:11:00 +0300

    Delete source directory@KATAKPIRANG66

--- source/assets/javascripts/all.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-  var select = document.querySelector('.locales select');
-  select.addEventListener('change', function(event){
-    origin = window.location.origin;
-    languageCode = event.currentTarget.selectedOptions[0].value
-    window.location.replace(origin + "/" + languageCode)
-  });
-});
